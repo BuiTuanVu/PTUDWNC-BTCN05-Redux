2ebc32d6cf7662de9729aa2ffc5409d83f0de8ec
BuiTuanVu <vubui2010@gmail.com>
2019-10-11 21:40:50 +0700

    Updates

---
+++ precache-manifest.ba8ff289553a6260ad43cd61669b2628.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fea0012b871cc11e42187cffca56e05",
+    "url": "/PTUDWNC-BTCN05-Redux/index.html"
+  },
+  {
+    "revision": "20462688cd17b915283a",
+    "url": "/PTUDWNC-BTCN05-Redux/static/css/main.d4dd63bc.chunk.css"
+  },
+  {
+    "revision": "e0c1e741e76c58656110",
+    "url": "/PTUDWNC-BTCN05-Redux/static/js/2.67b309b0.chunk.js"
+  },
+  {
+    "revision": "20462688cd17b915283a",
+    "url": "/PTUDWNC-BTCN05-Redux/static/js/main.0ec6566c.chunk.js"
+  },
+  {
+    "revision": "1341dd5ef63c935c5bbb",
+    "url": "/PTUDWNC-BTCN05-Redux/static/js/runtime-main.f4138a14.js"
+  }
+]);
